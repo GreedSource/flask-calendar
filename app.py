58cc0fd3863bf22048089e0a2043a29329533bfe
GreedSource <joelgarciia95@outlook.com>
2019-11-07 17:14:15 -0600

    cambios que no recuerdo

--- app.py
+++ app.py
@@ -11,11 +11,7 @@ ALLOWED_EXTENSIONS = set(['svg'])
 
 @app.route("/")
 def home():
-    return render_template("master.html")
-
-@app.route("/index")
-def index():
-    return render_template("master.html")
+    return render_template("home.html")
 
 @app.route('/subir')
 def upload():
