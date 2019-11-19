2245cd474956bf11424300ad4bd4934985577a7b
GreedSource <joelgarciia95@outlook.com>
2019-11-19 17:47:48 -0600

    cambios de clase xlsx

--- classes/xlsx.py
+++ classes/xlsx.py
@@ -1,10 +1,12 @@
-import xlrd
-import json
+from datetime import datetime
+import xlrd, json
 
 
 class xlsx_reader(object):
     def json_output(self, data):
-        with open('output/result.json', 'w', encoding='utf-8') as f:
+        date = datetime.now()
+        name = 'calendario'+ date.strftime('%Y%m%d-%H%M%S')
+        with open(f'output/{name}.json', 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         return True
         
@@ -13,15 +15,19 @@ class xlsx_reader(object):
         excel = xlrd.open_workbook(file)
         sheet_0 = excel.sheet_by_index(0) # Open the first tab
         prev_row = [None for i in range(sheet_0.ncols)]
+        first = True
         for row_index in range(sheet_0.nrows):
-            row = []
-            for col_index in range(sheet_0.ncols):
-                value = sheet_0.cell(rowx=row_index,colx=col_index).value
-                if len(value) == 0:
-                    value = prev_row[col_index]
-                row.append(value)
-            prev_row = row
-            all_data.append(row)
+            if first == False:
+                row = []
+                for col_index in range(sheet_0.ncols):
+                    value = sheet_0.cell(rowx=row_index,colx=col_index).value
+                    if len(value) == 0:
+                        value = prev_row[col_index]
+                    row.append(value)
+                prev_row = row
+                all_data.append(row)
+            else:
+                first = False
             ######
         data = []
         for row in all_data:
